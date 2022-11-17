2025b70e657f29ba60eb1a72e1909a6ae88a2a84
YourName <noor11809027@gmail.com>
2022-11-17 19:47:04 +0600

    more excercise add

---
+++ Exercise/excercise23.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class excercise23
+    {
+        public bool IsPalindrome(int x)
+        {
+            var number = x.ToString();
+            var reverse = "";
+
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                reverse += number[i].ToString();
+            }
+            if (number == reverse)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Exercise/excercise25.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class excercise25
+    {
+        public List<string> FizzBuzz(int n)
+        {
+            var list = new List<string>();
+
+            for (int a = 1; a <= n; a++)
+            {
+
+                if (a % 3 == 0 && a % 5 == 0)
+                {
+                    list.Add("FizzBuzz");
+                }
+                else if (a % 3 == 0)
+                {
+                    list.Add("Fizz");
+                }
+                else if (a % 5 == 0)
+                {
+                    list.Add("Buzz");
+                }
+                else
+                {
+                    list.Add(a.ToString());
+                }
+            }
+            return list;
+        }
+    }
+}
